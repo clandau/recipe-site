8cb9fb7a20eeb2436faf746e1b2c71d6c03cd098
courtney l <clandau@learnics.com>
2020-10-11 18:57:31 -0400

    pass through css

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("recipes/image");
+  eleventyConfig.addPassthroughCopy("css");
   // eleventyConfig.setFrontMatterParsingOptions({
   //   excerpt: true,
   //   excerpt_separator: "\n"
