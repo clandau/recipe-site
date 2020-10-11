9b317c950ee94ba7cbe0fc503d7773080d2983e3
courtney l <clandau@learnics.com>
2020-10-11 18:38:57 -0400

    trying to get bootstrap to work

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("recipes/image");
-  eleventyConfig.addPassthroughCopy("css");
   // eleventyConfig.setFrontMatterParsingOptions({
   //   excerpt: true,
   //   excerpt_separator: "\n"
